1b56ddab4422597e3076b137cef0daccb1ae03c1
unknown <chikviladze555@yahoo.com>
2024-06-05 16:27:09 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Task 1
+
+console.log(`Out${typeof (8 / 2)}`);
+console.log(`${typeof (8 / 2)}less`);
+console.log(`${16 / 8} Pac`);
+console.log(`Non${undefined / 4}nual`);
+console.log(`${typeof []}ivity`);
+
+// Task 2
+
+console.log((35).toString(2));
+console.log((23).toString(2));
+console.log((9878).toString(2));
