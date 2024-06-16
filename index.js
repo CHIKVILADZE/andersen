67571c1ea899e780c41749ee87afbf000f9aa211
unknown <chikviladze555@yahoo.com>
2024-06-16 15:41:19 +0400

    LECTURE 4 (Prototypes. Introduction to classes)

--- index.js
+++ index.js
@@ -56,73 +56,151 @@
 
 // Task 1
 
-const myIterable = {
-  from: 1,
-  to: 4,
-  [Symbol.iterator]: function () {
-    if (typeof this.from !== "number" || typeof this.to !== "number") {
-      throw new Error("from and to must be numbers");
-    }
+// const myIterable = {
+//   from: 1,
+//   to: 43,
+//   [Symbol.iterator]: function () {
+//     if (typeof this.from !== "number" || typeof this.to !== "number") {
+//       throw new Error("from and to must be numbers");
+//     }
 
-    if (this.to < this.from) {
-      throw new Error("to must be greater than or equal to from");
-    }
+//     if (this.to < this.from) {
+//       throw new Error("to must be greater than or equal to from");
+//     }
 
-    let current = this.from;
-    const last = this.to;
+//     let current = this.from;
+//     const last = this.to;
 
-    const next = () => {
-      if (current <= last) {
-        return { done: false, value: current++ };
-      } else {
-        return { done: true };
-      }
-    };
+//     const next = () => {
+//       if (current <= last) {
+//         return { done: false, value: current++ };
+//       } else {
+//         return { done: true };
+//       }
+//     };
 
-    return { next };
-  },
-};
+//     return { next };
+//   },
+// };
 
-for (let item of myIterable) {
-  console.log(item);
-}
+// for (let item of myIterable) {
+//   console.log(item);
+// }
 
-//  Task 2
+// //  Task 2
 
-function getPersons(name, age) {
-  //  Standard object literal
-  const person1 = {
-    name,
-    age,
-  };
+// function getPersons(name, age) {
+//   //  Standard object literal
+//   const person1 = {
+//     name,
+//     age,
+//   };
 
-  //  Using Object.assign to copy properties
-  const person2 = Object.assign({}, { name, age });
+//   //  Using Object.assign to copy properties
+//   const person2 = Object.assign({}, { name, age });
 
-  //  Using Object.create with prototype
-  const prototype = { name: "", age: 0 };
-  const person3 = Object.create(prototype);
-  person3.name = name;
-  person3.age = age;
+//   //  Using Object.create with prototype
+//   const prototype = { name: "", age: 0 };
+//   const person3 = Object.create(prototype);
+//   person3.name = name;
+//   person3.age = age;
 
-  //  Using a constructor function
-  function PersonConstructor(name, age) {
-    this.name = name;
-    this.age = age;
+//   //  Using a constructor function
+//   function PersonConstructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   const person4 = new PersonConstructor(name, age);
+
+//   //Using class syntax
+//   class PersonClass {
+//     constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//     }
+//   }
+//   const person5 = new PersonClass(name, age);
+
+//   return [person1, person2, person3, person4, person5];
+// }
+
+// const persons = getPersons("John Doe", 30);
+// console.log(persons);
+
+//        LECTURE 4 (Prototypes. Introduction to classes)
+
+// Task 1
+
+class Calculator {
+  constructor(x, y) {
+    this.validateNumber(x);
+    this.validateNumber(y);
+    this.x = x;
+    this.y = y;
+
+    this.logSum = this.logSum.bind(this);
+    this.logMul = this.logMul.bind(this);
+    this.logSub = this.logSub.bind(this);
+    this.logDiv = this.logDiv.bind(this);
   }
-  const person4 = new PersonConstructor(name, age);
 
-  //Using class syntax
-  class PersonClass {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
+  validateNumber(num) {
+    if (typeof num !== "number" || !isFinite(num)) {
+      throw new Error("Invalid number");
     }
   }
-  const person5 = new PersonClass(name, age);
 
-  return [person1, person2, person3, person4, person5];
+  setX(x) {
+    this.validateNumber(x);
+    this.x = x;
+  }
+
+  setY(y) {
+    this.validateNumber(y);
+    this.y = y;
+  }
+
+  logSum() {
+    return this.x + this.y;
+  }
+
+  logMul() {
+    return this.x * this.y;
+  }
+
+  logSub() {
+    return this.x - this.y;
+  }
+
+  logDiv() {
+    if (this.y === 0) {
+      throw new Error("Division by zero is incorrect");
+    }
+    return this.x / this.y;
+  }
 }
+try {
+  const calculator = new Calculator(10, "a");
 
-const persons = getPersons("John Doe", 30);
-console.log(persons);
+  const logSumRef = calculator.logSum;
+  console.log(logSumRef());
+
+  const logMulRef = calculator.logMul;
+  console.log(logMulRef());
+
+  const logSubRef = calculator.logSub;
+  console.log(logSubRef());
+
+  const logDivRef = calculator.logDiv;
+  console.log(logDivRef());
+
+  calculator.setX(20);
+  calculator.setY(4);
+
+  console.log(logSumRef());
+  console.log(logMulRef());
+  console.log(logSubRef());
+  console.log(logDivRef());
+} catch (error) {
+  console.error(error.message);
+}
